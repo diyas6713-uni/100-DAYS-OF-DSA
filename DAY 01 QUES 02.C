33d3dacd17d539fca655816f4b1ce4f28bb1c3b2
diyas6713-uni <diyas6713@gmail.com>
2026-02-02 09:05:00 +0530

    Day 1 - Q2: DSA Problem 2

---
+++ DAY 01 QUES 02.C
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+void reverse(int arr[], int n) {
+    int start = 0, end = n - 1, temp;
+    while(start < end) {
+        temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    reverse(arr, n);
+    
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+        
+    return 0;
+}
