2bb99a43160bd43676520627e2282c06dcfc08ea
diyas6713-uni <diyas6713@gmail.com>
2026-02-04 11:07:00 +0530

    Day 3 - Q2: DSA Problem 6

--- DAY 03 QUES 02.c
+++ DAY 03 QUES 02.c
@@ -1,5 +1,23 @@
-class Solution:
-    def missingNum(self, arr):
-        n = len(arr) + 1
-        total_sum = n * (n + 1)
-        return total_sum - sum(arr)
+#include <stdio.h>
+
+int maxSubArraySum(int arr[], int n) {
+    int max_so_far = arr[0], curr_max = arr[0];
+    
+    for(int i = 1; i < n; i++) {
+        curr_max = (arr[i] > curr_max + arr[i]) ? arr[i] : curr_max + arr[i];
+        
+        if(curr_max > max_so_far)
+            max_so_far = curr_max;
+    }
+    
+    return max_so_far;
+}
+
+int main() {
+    int arr[] = {-2, -3, 4, -1, -2, 1, 5, -3};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    
+    printf("%d", maxSubArraySum(arr, n));
+    
+    return 0;
+}
