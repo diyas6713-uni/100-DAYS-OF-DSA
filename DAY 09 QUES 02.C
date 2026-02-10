8a2d027fc87e2bb3c3018c428b9f0dfab7ced08c
diyas6713-uni <diyas6713@gmail.com>
2026-02-10 17:13:00 +0530

    Day 9 - Q2: DSA Problem 18

---
+++ DAY 09 QUES 02.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int factorial(int n) {
+    int res = 1;
+    for(int i = 2; i <= n; i++)
+        res *= i;
+    return res;
+}
+
+int main() {
+    int n = 5;
+    printf("%d", factorial(n));
+    return 0;
+}
