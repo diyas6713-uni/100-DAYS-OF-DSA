8546a4487cb67c871883c7688ffdf1064098ea44
diyas6713-uni <diyas6713@gmail.com>
2026-02-22 11:25:00 +0530

    Day 21 - Q2: DSA Problem 42

---
+++ DAY 20 QUES 02.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int firstOccurrence(int arr[], int n, int key) {
+    int l = 0, r = n - 1, res = -1;
+
+    while(l <= r) {
+        int mid = (l + r) / 2;
+        if(arr[mid] == key) {
+            res = mid;
+            r = mid - 1;
+        }
+        else if(arr[mid] < key)
+            l = mid + 1;
+        else
+            r = mid - 1;
+    }
+    return res;
+}
+
+int main() {
+    int arr[] = {1,2,2,2,3,4};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int key = 2;
+    printf("%d", firstOccurrence(arr, n, key));
+    return 0;
+}
