9d2db483ffee5f0b0a5eaa15dd6e2b33fe637268
diyas6713-uni <diyas6713@gmail.com>
2026-03-01 09:32:00 +0530

    Day 28 - Q2: DSA Problem 56

---
+++ DAY 27 QUES 02.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void selectionSort(int arr[], int n) {
+    for(int i = 0; i < n-1; i++) {
+        int min = i;
+        for(int j = i+1; j < n; j++) {
+            if(arr[j] < arr[min])
+                min = j;
+        }
+        int t = arr[i];
+        arr[i] = arr[min];
+        arr[min] = t;
+    }
+}
+
+int main() {
+    int arr[] = {64, 25, 12, 22, 11};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    selectionSort(arr, n);
+
+    for(int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
