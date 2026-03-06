d3d5df76986f4566518b478b92655b2946c5f91b
diyas6713-uni <diyas6713@gmail.com>
2026-03-06 14:37:00 +0530

    Day 33 - Q2: DSA Problem 66

---
+++ DAY 32 QUES 02.C
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int interpolationSearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while(low <= high && key >= arr[low] && key <= arr[high]) {
+        int pos = low + ((double)(high - low) / (arr[high] - arr[low])) * (key - arr[low]);
+
+        if(arr[pos] == key)
+            return pos;
+
+        if(arr[pos] < key)
+            low = pos + 1;
+        else
+            high = pos - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int arr[] = {10,20,30,40,50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int key = 40;
+    printf("%d", interpolationSearch(arr, n, key));
+    return 0;
+}
