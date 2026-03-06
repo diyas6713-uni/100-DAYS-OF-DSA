eff4efc66baa1576bdf833956448cb1cf0577b8c
diyas6713-uni <diyas6713@gmail.com>
2026-03-06 14:37:00 +0530

    Day 33 - Q1: DSA Problem 65

---
+++ DAY 32 QUES 01.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int key) {
+    for(int i = 0; i < n; i++)
+        if(arr[i] == key)
+            return i;
+    return -1;
+}
+
+int main() {
+    int arr[] = {10,20,30,40,50};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int key = 30;
+    printf("%d", linearSearch(arr, n, key));
+    return 0;
+}
