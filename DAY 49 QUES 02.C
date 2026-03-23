9cc72abf575b630ec86d5553abde038da9eeca67
diyas6713-uni <diyas6713@gmail.com>
2026-03-23 13:54:00 +0530

    Day 50 - Q2: DSA Problem 100

---
+++ DAY 49 QUES 02.C
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void fibIter(int n) {
+    int a = 0, b = 1, c;
+    for(int i = 0; i < n; i++) {
+        printf("%d ", a);
+        c = a + b;
+        a = b;
+        b = c;
+    }
+}
+
+int main() {
+    fibIter(7);
+    return 0;
+}
