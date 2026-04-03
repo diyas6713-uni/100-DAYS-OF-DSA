db68174768075c9fdb9c2b94b7185c3da717a302
diyas6713-uni <diyas6713@gmail.com>
2026-04-03 15:15:00 +0530

    Day 61 - Q1: DSA Problem 121

---
+++ DAY 60 QUES 01.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int sumArr(int arr[], int n) {
+    int sum=0;
+    for(int i=0;i<n;i++)
+        sum+=arr[i];
+    return sum;
+}
+
+int main() {
+    int arr[]={1,2,3,4,5};
+    printf("%d", sumArr(arr,5));
+    return 0;
+}
