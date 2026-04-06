2641a78b015cb40a0c22bbe5f16eecb2fad843bc
diyas6713-uni <diyas6713@gmail.com>
2026-04-06 09:18:00 +0530

    Day 64 - Q2: DSA Problem 128

---
+++ DAY 63 QUES 02.C
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void selection(int arr[], int n){
+    for(int i=0;i<n-1;i++){
+        int min=i;
+        for(int j=i+1;j<n;j++)
+            if(arr[j]<arr[min]) min=j;
+        int t=arr[i];
+        arr[i]=arr[min];
+        arr[min]=t;
+    }
+}
+
+int main(){
+    int arr[]={64,25,12,22,11};
+    selection(arr,5);
+    for(int i=0;i<5;i++) printf("%d ",arr[i]);
+    return 0;
+}
