1770128a5fe8dcfb6dad1f73d52b03a019cf9ce1
diyas6713-uni <diyas6713@gmail.com>
2026-04-20 14:32:00 +0530

    Day 78 - Q2: DSA Problem 156

---
+++ DAY 77 QUES 02.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int diagonalSum(int mat[3][3]){
+    int sum=0;
+    for(int i=0;i<3;i++)
+        sum+=mat[i][i];
+    return sum;
+}
+
+int main(){
+    int mat[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+    printf("%d", diagonalSum(mat));
+    return 0;
+}
