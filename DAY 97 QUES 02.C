1fb591b46fff50f3e34775778d09f14b450219fc
diyas6713-uni <diyas6713@gmail.com>
2026-05-02 17:44:00 +0530

    Day 90 - Q18: DSA Problem 196

---
+++ DAY 97 QUES 02.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int max2D(int mat[2][2]){
+    int max=mat[0][0];
+    for(int i=0;i<2;i++)
+        for(int j=0;j<2;j++)
+            if(mat[i][j]>max) max=mat[i][j];
+    return max;
+}
+
+int main(){
+    int mat[2][2]={{1,9},{3,4}};
+    printf("%d", max2D(mat));
+    return 0;
+}
