72205c205de2a055c904e9fd4e65c0cf03963aab
diyas6713-uni <diyas6713@gmail.com>
2026-05-02 17:44:00 +0530

    Day 90 - Q20: DSA Problem 198

---
+++ DAY 98 QUES 02.C
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void transpose2D(int mat[2][2]){
+    for(int i=0;i<2;i++){
+        for(int j=i+1;j<2;j++){
+            int t=mat[i][j];
+            mat[i][j]=mat[j][i];
+            mat[j][i]=t;
+        }
+    }
+}
+
+int main(){
+    int mat[2][2]={{1,2},{3,4}};
+    transpose2D(mat);
+    for(int i=0;i<2;i++){
+        for(int j=0;j<2;j++)
+            printf("%d ",mat[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
